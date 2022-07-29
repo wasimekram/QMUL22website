93a8673f02ab9c5c9e151b0200257266875f4b1e
Wasim <wasimekram69@gmail.com>
2022-07-29 20:13:16 +0100

    fixed carousel gif

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 #from sqlalchemy import false, true
 from scripts.twolink import twolink
+import glob
+import os
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -8,20 +10,26 @@ app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
 
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+    #code for carousel    
+    script_dir = os.path.dirname(__file__)
+    results_dir = os.path.join(script_dir + os.sep + "static" + os.sep + "images" + os.sep + "results" + os.sep)
+    imgs = glob.glob(results_dir + "/*.png")
+
     if request.method == 'GET':
-        return render_template('main.html')
+        return render_template('main.html', imgs=imgs)
     if request.method == 'POST':
         armfirst = request.form.get('armfirst')
         data = request.form.to_dict()
         print(data)
         try:
             twolink(data)
-            return render_template('main.html', test=data)
+            return render_template('main.html', test=data, imgs=imgs)
         except Exception as e: 
             print(e)
-            return render_template('main.html', test=data, e=e)
+            return render_template('main.html', test=data, imgs=imgs, e=e)
        
 
 
