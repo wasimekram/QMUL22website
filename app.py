8424e04f5dc8db6a6ca68ee7b2b2c53715fb8f94
Wasim <wasimekram69@gmail.com>
2022-07-29 10:29:01 +0100

    updated templates

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
 
 @app.route('/')
 def index():
