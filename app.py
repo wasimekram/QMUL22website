f715b98fd27e5dcb21414a779b94411e34722697
Wasim <wasimekram69@gmail.com>
2022-08-05 14:05:15 +0100

    added plot#

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import os
 import pandas as pd
 import json
 import plotly
-import plotly.express as px
+import plotly.express as px#
+import numpy as np
+import plotly.graph_objects as go
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -24,9 +26,27 @@ def index():
     df = pd.DataFrame({'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 'Bananas'],  'Amount': [4, 1, 2, 2, 4, 5], 'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']})   
     fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')   
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    N=100
+    random_x = np.linspace(0, 5, N)
+    random_y0 = np.random.randn(N) + 5
+    random_y1 = np.random.randn(N)
+    random_y2 = np.random.randn(N) - 5
+    fig = go.Figure()
+    # Add traces
+    fig.add_trace(go.Scatter(x=random_x, y=random_y0,
+                    mode='lines+markers',
+                    name='lines+markers'))
+    fig.add_trace(go.Scatter(x=random_x, y=random_y1,
+                    mode='markers',
+                    name='markers'))
+    fig.add_trace(go.Scatter(x=random_x, y=random_y2,
+                    mode='lines',
+                    name='lines'))
+    graphJSON1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
 
     if request.method == 'GET':
-        return render_template('main.html', imgs=imgs, graphJSON=graphJSON)
+        return render_template('main.html', imgs=imgs, graphJSON=graphJSON, graphJSON1=graphJSON1)
     if request.method == 'POST':
         armfirst = request.form.get('armfirst')
         data = request.form.to_dict()
